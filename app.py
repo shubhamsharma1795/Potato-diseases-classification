dd9ecdbbbb5f44d0f10a5591757350e3d89c51f5
shubhamsharma1795 <154001207+shubhamsharma1795@users.noreply.github.com>
2024-04-16 11:58:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,10 @@ def main():
     try:
         model_path = os.path.abspath('potatoes.h5')
         model = load_model(model_path)
-
+    except Exception as e:
+        st.error("Error loading model. Please check the model file path and try again.")
+        
+    
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image', use_column_width=True)
